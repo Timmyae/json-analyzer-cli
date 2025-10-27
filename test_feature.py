8d67f87a4bf015ab43afc1d9c2148e27c6db3940
Dr.Yousef <173801249+Timmyae@users.noreply.github.com>
2025-10-27 23:38:17 +0300

    Add test_feature.py - Testing file creation from scratch

---
+++ test_feature.py
@@ -0,0 +1,10 @@
+# Test Feature
+# This is a test file to demonstrate creating new files
+
+def test_function():
+    """Test function to verify file creation"""
+    print('File creation test successful')
+    return True
+
+if __name__ == '__main__':
+    test_function()
